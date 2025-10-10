5c10ee08839becd0656bf9a861dd783421c1b16d
Lee ByeongJun <lbj199874@gmail.com>
2025-10-10 13:24:59 +0900

    Update packages/uri-template/tsdown.config.ts
    Co-authored-by: Hong Minhee (洪 民憙) <hong@minhee.org>

--- packages/uri-template/tsdown.config.ts
+++ packages/uri-template/tsdown.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/*.spec.ts"],
+  unbundle: true,
   dts: true,
   platform: "neutral",
   format: ["esm", "cjs"],
