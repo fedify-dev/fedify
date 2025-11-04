51910dfbe6df2f9fa956e55f005b54a8322fa5c8
Hyeonseo Kim <dodok8@gmail.com>
2025-11-04 16:29:00 +0900

    Make external @fedify/fedify

--- examples/fresh/vite.config.ts
+++ examples/fresh/vite.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from "vite";
 import { fresh } from "@fresh/plugin-vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [fresh()],
+  ssr: {
+    external: [
+      "@fedify/fedify",
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "@fedify/fedify",
+      ],
+    },
+  },
 });
