072299e948e9ebfe0fb2946412c7293ab2c55244
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-11-06 08:17:52 +0000

    Added `filterOptions`

--- packages/cli/src/init/test/run.ts
+++ packages/cli/src/init/test/run.ts
@@ -1,8 +1,8 @@
-import { map, pipe, tap } from "@fxts/core";
+import { filter, map, pipe, tap } from "@fxts/core";
 import { optionNames } from "@optique/core";
 import { join } from "node:path";
 import { printMessage } from "../../utils.ts";
-import { createTestApp, generateTestCases } from "./create.ts";
+import createTestApp, { filterOptions, generateTestCases } from "./create.ts";
 import runServerAndReadUser from "./lookup.ts";
 import type { InitTestData } from "./types.ts";
 
@@ -18,6 +18,7 @@ export const runTests =
       options,
       printStartMessage,
       generateTestCases,
+      filter(filterOptions),
       map(createTestApp(join(testDirPrefix, getMid(dryRun, hydRun, dry)), dry)),
       Array.fromAsync<string>,
       runServerAndReadUser,
