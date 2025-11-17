2cb62588b5d7a9fded664d86e837aaa97f02e30f
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-11-17 06:01:02 +0000

    Fixed dry/hyd test order, show test mode and refactored related code

--- packages/cli/src/init/test/run.ts
+++ packages/cli/src/init/test/run.ts
@@ -6,37 +6,34 @@ import createTestApp, { filterOptions, generateTestCases } from "./create.ts";
 import runServerAndLookupUser from "./lookup.ts";
 import type { InitTestData } from "./types.ts";
 
-export const isDryRun = <T extends { dryRun: boolean }>({ dryRun }: T) =>
-  dryRun;
-export const isHydRun = <T extends { hydRun: boolean }>({ hydRun }: T) =>
-  hydRun;
-
-export const runTests =
+const runTests =
   (dry: boolean) =>
   <T extends InitTestData>({ testDirPrefix, dryRun, hydRun, ...options }: T) =>
     pipe(
       options,
-      printStartMessage,
+      printStartMessage(dry),
       generateTestCases,
       filter(filterOptions),
       map(createTestApp(join(testDirPrefix, getMid(dryRun, hydRun, dry)), dry)),
       Array.fromAsync<string>,
       unless(always(dry), runServerAndLookupUser),
     );
+export default runTests;
 
-const printStartMessage: <T>(t: T) => T = tap(
-  () =>
-    printMessage`\n
-Init Test start!
+const printStartMessage: (dry: boolean) => <T>(t: T) => T = (dry: boolean) =>
+  tap(
+    () =>
+      printMessage`\n
+Init ${dry ? "Dry" : "Hyd"} Test start!
 Options: ${
-      optionNames([
-        "Web Framework",
-        "Package Manager",
-        "KV Store",
-        "Message Queue",
-      ])
-    }`,
-);
+        optionNames([
+          "Web Framework",
+          "Package Manager",
+          "KV Store",
+          "Message Queue",
+        ])
+      }`,
+  );
 
 const getMid = (dryRun: boolean, hydRun: boolean, dry: boolean) =>
   dryRun === hydRun ? dry ? "dry" : "hyd" : "";
