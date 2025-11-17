53cba2861b5262d14cf998e3059198ee99bb1634
Kim, Hyeonseo <dodok8@gmail.com>
2025-11-17 23:52:01 +0900

    refactor: remove unused components and middleware from fresh example

--- examples/fresh/main.ts
+++ examples/fresh/main.ts
@@ -7,20 +7,6 @@ export const app = new App<State>();
 
 app.use(staticFiles());
 
-// Pass a shared value from a middleware
-app.use(async (ctx) => {
-  ctx.state.shared = "hello";
-  return await ctx.next();
-});
-
-// this is the same as the /api/:name route defined via a file. feel free to delete this!
-app.get("/api2/:name", (ctx) => {
-  const name = ctx.params.name;
-  return new Response(
-    `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
-  );
-});
-
 // Include file-system based routes here
 app.fsRoutes();
 
--- examples/fresh/routes/index.tsx
+++ examples/fresh/routes/index.tsx
@@ -1,11 +1,6 @@
-import { useSignal } from "@preact/signals";
 import { Head } from "fresh/runtime";
 import { define } from "../utils.ts";
-import Counter from "../islands/Counter.tsx";
-
 export default define.page(function Home(ctx) {
-  const count = useSignal(3);
-
   console.log("Shared value " + ctx.state.shared);
 
   return (
@@ -19,14 +14,12 @@ export default define.page(function Home(ctx) {
           src="/logo.svg"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="the Fedify logo"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Welcome to Fresh & Fedify</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          Hello, Fediverse!
         </p>
-        <Counter count={count} />
       </div>
     </div>
   );
