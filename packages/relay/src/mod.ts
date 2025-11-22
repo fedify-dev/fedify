10155f9852db6ab4bf1405afd60d35671254ed55
Jiwon Kwon <kjiwon411@gmail.com>
2025-11-23 01:21:59 +0900

    refactor(relay): add create factory function for mastodon and litepub

--- packages/relay/src/mod.ts
+++ packages/relay/src/mod.ts
@@ -11,9 +11,11 @@
 // Export relay functionality here
 export {
   createRelay,
+  type LitePubFollower,
   RELAY_SERVER_ACTOR,
   type RelayOptions,
   type SubscriptionRequestHandler,
 } from "./relay.ts";
 
 export { MastodonRelay } from "./mastodon.ts";
+export { LitePubRelay } from "./litepub.ts";
