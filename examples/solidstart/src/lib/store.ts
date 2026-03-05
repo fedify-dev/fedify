4e307c1f742cc50a55f17952b121755a3aca339e
Kim, Hyeonseo <dodok8@gmail.com>
2026-03-05 18:10:32 +0900

    Format all file

--- examples/solidstart/src/lib/store.ts
+++ examples/solidstart/src/lib/store.ts
@@ -7,8 +7,8 @@ declare global {
 
 export const keyPairsStore: Map<string, CryptoKeyPair[]> =
   globalThis.keyPairsStore ?? new Map();
-export const relationStore: Map<string, Person> =
-  globalThis.relationStore ?? new Map();
+export const relationStore: Map<string, Person> = globalThis.relationStore ??
+  new Map();
 
 // This is just a hack for the demo.
 // Never do this in production; use safe and secure storage.
