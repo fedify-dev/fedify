a45344f9ed0f8cd44321da864ba814cc09130018
ChanHaeng Lee <2chanhaeng@gmail.com>
2026-04-15 08:00:50 +0000

    Replace `~~/server` to `#server`

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,8 +1,6 @@
 export default defineNuxtConfig({
   modules: ["@fedify/nuxt"],
-  fedify: {
-    federationModule: "~/server/federation",
-  },
+  fedify: { federationModule: "#server/federation" },
   ssr: true,
   devServer: { host: "0.0.0.0" },
   vite: { server: { allowedHosts: true } },
